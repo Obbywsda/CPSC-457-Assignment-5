e5bec3a512dd7aedfeee35ff137a3bb22cae7241
CroyDobson <croy.dobson@ucalgary.ca>
2025-11-23 10:55:58 -0700

    Finished initial code, must add comments and error test

--- A5P2.java
+++ A5P2.java
@@ -14,16 +14,29 @@ class PrimeFinder extends Thread {
 
 
     public static void main (String [] args) {
+        if (args.length != 3){
+            System.out.println("Usage: java PrimeFinder <min> <max> <thread_count>");
+            System.exit(0);
+        }
         int lower = Integer.parseInt(args[0]);
         int upper = Integer.parseInt(args[1]);
         int count = Integer.parseInt(args[2]);
+        if (upper<lower){
+            System.out.println("The argument <max> must be greater than <min>");
+            System.exit(0);
+        }
+        if (count<1){
+            System.out.println("The argument <thread_count> must be greater than 1");
+            System.exit(0);
+        }
         int dif = upper - lower;
+        if (dif<count){
+            count=dif;
+        }
         int extra = dif%count;
         int inc = (dif-extra)/count;
         a = new PrimeNumbers(lower, upper);
-
-        
-
+        System.out.println();
         for (int i = lower; i <= upper; i++) {
             int temp = i+inc;
             if (extra>0){
@@ -31,9 +44,10 @@ class PrimeFinder extends Thread {
                 temp++;
             }
             running.add(new PrimeFinder(i + " - " +temp,i,temp));
+            System.out.println("Thread "+(running.size()-1)+" searching range ["+i+", "+temp+"]");
             i = temp;
-            System.out.println();
         }
+        System.out.println();
         boolean isRunning = true;
         while (isRunning){
             isRunning = false;
@@ -41,8 +55,18 @@ class PrimeFinder extends Thread {
                 isRunning = isRunning || thread.isAlive();
             }
         }
-
-
+        System.out.println("Main Thread: All workers finished. Primes found:");
+        a.sort();
+        int u = 0;
+        for (int i : a.getPrimeNumbers()){
+            u++;
+            System.out.print(i);
+            if (u != a.getLength()){
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        System.out.println("Main Thread: "+a.getLength()+" prime numbers found.");
     }
 
     PrimeFinder (String thread_name, int min, int max) {
@@ -83,6 +107,10 @@ class PrimeFinder extends Thread {
 
         public int getLength(){
             return this.primeNumbers.size();
+        }
+
+        public void sort() {
+            this.primeNumbers.sort(null);
         }
     }
 
